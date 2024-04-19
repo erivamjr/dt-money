0016ff5f78db934dce1c42f976366b07401f2733
José Erivam <erivamdev@gmail.com>
2024-04-19 16:15:52 -0300

    :wrench: chore: preparing the developer environment.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
 
 
   return (
-    <>
-      <div>
-        <h1>Principal</h1>
-      </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
 
-    </>
+      <h1>Principal</h1>
+    </ThemeProvider>
   )
 }
 
