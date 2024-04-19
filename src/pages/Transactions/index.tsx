0dd4f31fc60746fe06eaea217ab3f3ca2769a29d
José Erivam <erivamdev@gmail.com>
2024-04-19 17:27:20 -0300

    :sparkles: feat: add components transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,40 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de website</td>
+              <td >
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Aluguel de apartamento</td>
+              <td ><PriceHighlight variant="outcome">- R$ 1.000,00</PriceHighlight></td>
+              <td>Casa</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Computador</td>
+              <td ><PriceHighlight variant="outcome">- R$ 5.000,00</PriceHighlight></td>
+              <td>Eletrônicos</td>
+              <td>10/02/2021</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
