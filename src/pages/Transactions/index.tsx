d160a9435c197a9fb8de31af2a794374aa0168a0
José Erivam <erivamdev@gmail.com>
2024-04-21 20:04:43 -0300

    :sparkles: feat: add server and integrating

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+    setTransactions(data)
+  }
+  useEffect(() => {
+    loadTransactions()
+  }
+    , [])
+
   return (
     <div>
       <Header />
@@ -14,26 +35,18 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width={'50%'}>Desenvolvimento de website</td>
-              <td >
-                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Desenvolvimento</td>
-              <td>10/02/2021</td>
-            </tr>
-            <tr>
-              <td width={'50%'}>Aluguel de apartamento</td>
-              <td ><PriceHighlight variant="outcome">- R$ 1.000,00</PriceHighlight></td>
-              <td>Casa</td>
-              <td>10/02/2021</td>
-            </tr>
-            <tr>
-              <td width={'50%'}>Computador</td>
-              <td ><PriceHighlight variant="outcome">- R$ 5.000,00</PriceHighlight></td>
-              <td>Eletrônicos</td>
-              <td>10/02/2021</td>
-            </tr>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td width={'50%'}>{transaction.description}</td>
+                <td >
+                  <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+            )}
+
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
